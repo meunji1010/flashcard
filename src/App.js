1d6270416dda87d21a7d3536ab4782aead76a3a0
meunji1010 <meunji1010@gmail.com>
2025-02-13 17:20:34 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import '@fortawesome/fontawesome-free/js/all.js'
+import { useState } from "react";
+import Home from "./components/Home";
+import mockData from "./data/mockData";
+import DetailList from "./components/DetailList";
+import Learning from "./components/Learning";
+import Result from "./components/Result";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [selected,setSelected] = useState(null);
+  const [learningMode,setLearningMode] = useState(false);
+  const [exitMode,setExitMode] = useState(false);
+  const handleSelected = (obj) => {
+    setSelected(obj);
+  }
+  const handleChangeMode = (data) =>{
+    setLearningMode(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+      <div className='head-bar'>
+        <p>14:30</p>
+        <div className='icon'>
+        <i class="fas fa-signal"></i>
+        <i class="fas fa-wifi"></i>
+        <i class="fas fa-battery-three-quarters"></i>
+        </div>
+      </div>
+      {
+        !selected &&
+      <Home categories={mockData.categories} onSelected={handleSelected}/>
+      }     
+      {
+        selected && !learningMode &&
+        <DetailList
+          selected={selected}
+          onSelected={handleSelected}
+          onChangeMode = {handleChangeMode}/>
+      }
+      {
+        learningMode && !exitMode &&
+        <Learning
+          flashcards = {selected.flashcards}
+          onExit={()=>{
+            setSelected(null);
+            setLearningMode(false);
+          }}
+          onResult={()=>{
+            setExitMode(true);
+          }}
+        />
+      }
+      {
+        exitMode &&
+        <Result 
+          onExit={()=>{
+            setSelected(null);
+            setLearningMode(false);
+            setExitMode(false);
+        }}/>
+      }
     </div>
   );
-}
+};
 
 export default App;
